5bcc3092206631d0feb96c8b49c060088963d45c
Christopher Drellow <christopherlives@gmail.com>
2013-02-26 14:31:49 -0800

    deleted files

--- spec/models/answer_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Answer do
-  @user = User.new(:username => "foo", :email => "foo@bar.com",
-                   :password => "password", :password_confirmation => "password")
-  @answer = Answer.create!(:user_id => 1, :body => "My answer")
-
-
-end
