cfb33e4f539632fb5f6d1e3956b29bac8b1f2566
Christopher Drellow <christopherlives@gmail.com>
2013-03-02 21:06:08 -0800

    sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Tq::Application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => "cdrellow",
-  :password => "$endgr1d",
-  :domain => "thetq.herokuapp.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
