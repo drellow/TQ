22f4beaae00caec2c8a8e918556e3bcfa99c8387
App Academy Student <christopherlives@gmail.com>
2013-03-17 14:00:30 -0700

    reconfigure show pages

--- app/models/question.rb
+++ app/models/question.rb
@@ -20,6 +20,18 @@ class Question < ActiveRecord::Base
   end
 
   def self.todays_answers_posted?
-    self.todays_question.posted_answers?
+    self.question_posted? and self.todays_question.posted_answers?
+  end
+
+  def sorted_answers(current_user)
+    answers = self.answers
+    if current_user.provider == "facebook"
+      answers.sort_by do |a|
+        [current_user.friends_with?(a.user) ? 0 : 1,
+          a.score * -1]
+      end
+    else
+      answers.sort_by { |a| a.score * -1}
+    end
   end
 end
