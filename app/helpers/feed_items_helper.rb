a264b8387a5683faa25ca0478b25186271933642
Christopher Drellow <christopherlives@gmail.com>
2013-04-13 15:40:22 -0700

    working on bullshit feeditems

--- app/helpers/feed_items_helper.rb
+++ app/helpers/feed_items_helper.rb
@@ -18,8 +18,18 @@ module FeedItemsHelper
         title << singular
       when :who
         title << "Who"
-      when :verb
-        title << verb
+      when :past
+        title << past_verb
+      when :present
+        title << present_verb
+      when :are
+        title << "are"
+      when :is
+        title << "is"
+      when :adv
+        title << adverb
+      when :user
+        title << User.all.sample.username
       end
     end
     final_title = title.join(" ")
@@ -29,7 +39,10 @@ module FeedItemsHelper
 
   def self.choose_format
     formats = [
-              [:the, :adj, :plural, :verb, :the, :adj, :plural]
+              [:the, :adj, :plural, :past, :the, :adj, :plural],
+              [:the, :adj, :plural, :are, :present],
+              [:sing, :is, :present, :adv],
+              [:user, :is, :present, :adv]
               ]
     formats.sample
   end
@@ -47,13 +60,13 @@ module FeedItemsHelper
   end
 
   def self.plural
-    %w(Monsters Dragons Doom Swelling Teeth Fright Chocolate Cupcakes
-       Coffee Airplanes Coding Computers Styrofoam Carpet Beer Waffles
-       Dolphins Couches Spoon Forks Magic Name_Brand_Desserts Stillness
+    %w(Monsters Dragons Doom Swelling Teeth Chocolate Cupcakes Presidents
+       Airplanes Computers Styrofoam Carpet Beer Waffles Melons Clocks
+       Dolphins Couches Spoon Forks Magic Name_Brand_Desserts 
        Ice Pork Handshakes Plastic Drainage Neckties Bubble_Gum Pickles
        Stains Swimming_Pools Bees Popcorn Trunks Trampolines Ketchup
-       Tea Mountain_Climbing Flat_Tires Sword_Fighting Car_Parts Soup 
-       Book_Reading Snacks Charts Presentations Jam).sample.humanize.downcase
+       Tea Flat_Tires Sword_Fighting Car_Parts Soup 
+       Snacks Charts Presentations Jam).sample.humanize.downcase
   end
 
   def self.singular
@@ -64,16 +77,42 @@ module FeedItemsHelper
        Monster Book Novel Storm Window Eye Donkey Apple Garlic
        Taco Burrito Cartoon Target Wheel Pipe Portal Peel Desk Monitor
        Torpedo Boy Man Friend Uncle Tailor Baker Mouse Player
-       Field Spaceman Dik-Dik Gun).sample.humanize.downcase
+       Field Spaceman Dik-Dik Gun Sasquatch).sample.humanize.downcase
   end
 
-  def self.verb
+  def self.past_verb
     %w(cried fell dunked cooked_shellfish ate_a_dog built coded painted
        drove rode killed Drinks read licked borrowed twirled_and_twirled
        destroyed created consumed threw clothed fed got_addicted_to
        emailed quilted dunked popped comparmentalized move bought paid piloted
        sold collected grew climbed clubbed struck hugged painted landed
-       kissed broke watched heard fixed saw conducted hid_behind).sample.humanize.downcase
+       kissed broke watched heard fixed saw conducted hid_behind played).sample.humanize.downcase
+  end
+  
+  def self.present_verb
+    %w(pillow_fighting rapping bombing_hills spraypainting feeling_good
+       swordfighting drinking_beers reading_books eating sitting_down
+       doing_nothing_really smiling picking_up_hitchhikers driving_a_truck
+       tuning_a_guitar seeing_something_weird seeing_someone_else making_out
+       drinking_port).sample.humanize.downcase
+  end
+  
+  def self.adverb
+    %w(mournfully direly sadly happily drunkenly boastfully badly
+       pitifully powerfully dourly melodically artfully cunningly
+       comfortably rudely peacefully painfully regularly stunningly
+       loudly kookily oddly clumsily coaxingly coincidentally coldly
+       colorfully commonly comfortably compactly daily daintily dangerously        daringly
+       darkly dastardly eloquently elegantly elsewhere emotionally endlessly
+       energetically enjoyably enormously finally financially finitely
+       fluently fondly immensely impatiently impressively inappropriately
+       incessantly incorrectly obediently obligingly obnoxiously obviously
+       occasionally patiently perfectly periodically perpetually persistently
+       personally sadly safely scarcely scarily scientifically searchingly
+       secretively securely sedately seemingly seldom selfishly selflessly
+       seriously shakily shamelessly sharply sheepishly shoddily shortly
+       shrilly significantly silently simply sincerely singularly shyly
+       uniformly unilaterally unimpressively unnaturally unnecessarily).sample.humanize.downcase
   end
   
 end
